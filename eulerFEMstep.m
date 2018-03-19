b85fa9375ef5273da90a1bd20e7b30675e1db82c
Erik Sandström <33764884+tfy14esa@users.noreply.github.com>
2018-03-19 02:42:04 -0700

    Delete eulerFEMstep.m

--- eulerFEMstep.m
+++
@@ -1,9 +0,0 @@
-function anew = eulerFEMstep(C, K, f, deltat, aold)
-%eulerFEM Computes the new nodal temperatures using implicit Euler
-%   C is the transient matrix
-%   K is the stiffness matrix (including convection)
-%   deltat is the time between aold and anew
-%   f is the RHS vector (from boundary conditions, flux and convection)
-
- anew = (C+deltat*K)\(C*aold+deltat*f);
-end
